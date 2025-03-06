a472a366c661a2cf8b526e92496af9cced49e0ae
marcel-blegh <m.r.jouvenaz@vu.nl>
2025-03-06 13:30:20 +0100

    first working version

---
+++ compare.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import ast
+
+
+savable = pd.read_csv("savable.csv", encoding='utf-8')
+'''
+savable is the only file where the doi doesn't start with https://doi.org/, so merges on doi wouldn't work.
+hence we add https://doi.org/ 
+'''
+savable['doi'].apply(lambda x: "https://doi.org/" + str(x) if x else x)
+
+altmetric = pd.read_csv("Altmetric.csv", encoding='utf-8')
+overton = pd.read_csv("overton_data.csv", encoding='utf-8')
+'''
+overton data has not been split into columns, so we have to do that here.
+add overton to the column name to make the final merge more intelligible.
+'''
+overton['overton_policy_doc_id'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("policy_document_id"))
+overton['overton_policy_source_id'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("policy_source_id"))
+overton['overton_source_title'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("source_title"))
+overton['overton_document_title'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("document_title"))
+overton['overton_published_on'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("published_on"))
+overton['overton_doc_url'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("document_url"))
+overton['overton_type'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("type"))
+overton['overton_subtype'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("subtype"))
+overton['overton_country'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("country"))
+overton['overton_topics'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("topics"))
+overton['overton_classifications'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("classifications"))
+overton['overton_url'] = overton['docs'].apply(lambda x: ast.literal_eval(x).get("url"))
+overton.drop(columns=['docs'], inplace=True)
+overton.rename(columns={'title': 'overton_title'}, inplace=True)
+
+middle = pd.merge(savable, altmetric, how='left', left_on='doi', right_on='DOI')
+final = pd.merge(middle, overton, how='left', on='doi')
+
+final.to_csv("final.csv", index=False)
---
+++ overton.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+import ast
+
+def url_reader(my_url):
+    global overton_data
+    response = requests.get(my_url)
+    for item in response.json()['results']['results']:
+        # print(result)
+        # item = ast.literal_eval(result)
+        doi = item['document_url']
+        title = item['title']
+        docs = item['cited_by_documents']
+        overton_data = pd.concat([overton_data, pd.DataFrame({'doi':doi, 'title':title, 'docs': docs})], ignore_index=True, axis=0)
+    return response.json()['query']['next_page_url']
+
+
+url = "https://app.overton.io/articles.php?identifiers=set%3A17121%3A91fab73070554c4b19f855d5f54085d3&sort=date&format=json&api_key=a49a2c-377135-1e2531"
+overton_data = pd.DataFrame(columns=['doi', 'title', 'docs'])
+
+while url :
+    url = url_reader(url)
+overton_data.to_csv('overton_data.csv', index=False)
